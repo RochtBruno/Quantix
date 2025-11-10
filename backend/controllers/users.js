b2722826cf7ad80703f6dc23addeec975d5c5ddc
Bruno <bruno.taveira.r0.382@gmail.com>
2025-11-10 13:12:39 -0300

    feat: controller da api de cadastrar e login de usuário implementada

---
+++ backend/controllers/users.js
@@ -0,0 +1,116 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+require('dotenv').config()
+import { PrismaClient } from '../src/generated/prisma/index.js'
+
+const prisma = new PrismaClient()
+
+export const signup = async (req, res) => {
+	try{
+		const [email, name, lastName, password] = req.body
+
+		if(!email || !name || lastName || !password){
+			return res.status(400).json({
+				error: "Campos vazios não são permitidos"
+			})
+		}
+
+		const userExists = await prisma.user.findUnique({
+			where: { email }
+		})
+		if(userExists){
+			return res.status(400).json({
+				error: "Email já cadastrado"
+			})
+		}
+
+		const hashedPassword = await bcrypt.hash(password,10)
+
+		const user = await prisma.user.create({
+			data:{
+				email,
+				name,
+				lastName,
+				password: hashedPassword
+			},
+			select:{
+				id: true,
+				email:true,
+				name: true,
+				lastName: true,
+				createdAt: true
+			}
+		})
+
+		const token = jwt.sign(
+			{ userId: user.id},
+			process.env.JWT_SECRET,
+			{ expiresIn: '7d'}
+		)
+
+		res.status(201).json({
+			message: "Usuário criado com sucesso",
+			token,
+			user
+		})
+	}catch(err){
+		res.status(500).json({
+			error: "Erro ao cadastrar usuário"
+		})
+	}
+}
+
+export const sign = async (req, res) => {
+	try{
+		const { email, password } = req.body
+		if(!email || !password){
+			return res.status(400).json({
+				error: "Email e senha são obrigatórios"
+			})
+		}
+
+		const user = await prisma.user.findUnique({
+			where: { email},
+			select: {
+				id: true,
+				email: true,
+				name: true,
+				lastName: true,
+				password: true,
+				createdAt: true
+			}
+		})
+
+		if(!user){
+			return res.status(401).json({
+				error: "Email ou senha incorretos"
+			})
+		}
+
+		const passwordMatch = await bcrypt.compare(password, user.password)
+
+		if(!passwordMatch){
+			return res.status(401).json({
+				error: 'Email ou senha incorretos'
+			})
+		}
+
+		const token = jwt.sign(
+			{ userId: user.id},
+			process.env.JWT_SECRET,
+			{ expiresIn: '7d'}
+		)
+
+		const { password: _, ...userWithoutPassword } = user
+
+		res.status(200).json({
+			message: "Login realizado com sucesso",
+			token,
+			user: userWithoutPassword
+		})
+	}catch(err){
+		res.status(500).json({
+			error: 'Erro ao fazer login'
+		})
+	}
+}
---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+const cors = require('cors')
+require("dotenv").config()
+const PORT = process.env.PORT
+
+app.use(express.json())
+app.use(cors())
+
+
+app.listen(PORT,()=>{
+	console.log(`Servidor rodando na porta ${PORT}`)
+})
